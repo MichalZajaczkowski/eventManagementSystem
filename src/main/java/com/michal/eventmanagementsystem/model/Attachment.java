cb6e92cb9a28953aa770d836aab2d968db85f843
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2022-04-09 14:51:20 +0200

    add Attachment Entity

---
+++ src/main/java/com/michal/eventmanagementsystem/model/Attachment.java
@@ -0,0 +1,40 @@
+package com.michal.eventmanagementsystem.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "attachment")
+public class Attachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long attachmentId;
+    private String fileName;
+    private Event event;
+
+    public Attachment() {
+    }
+
+    public Long getAttachmentId() {
+        return attachmentId;
+    }
+
+    public void setAttachmentId(Long attachmentId) {
+        this.attachmentId = attachmentId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+}
