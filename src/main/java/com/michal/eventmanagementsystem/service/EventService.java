291787577233e0466d0719440a939531c36aadab
Michał Zajączkowski <mzajaczkowski90@gmail.com>
2022-07-02 15:48:49 +0200

    change name for identification id

--- src/main/java/com/michal/eventmanagementsystem/service/EventService.java
+++ src/main/java/com/michal/eventmanagementsystem/service/EventService.java
@@ -38,20 +38,20 @@ public class EventService {
                 || (eventDto.getOrganizer() != null && eventDto.getOrganizer().getId() != null)
                 || (eventDto.getStatus() != null && eventDto.getStatus().getId() != null)
                 || (eventDto.getCategory() != null && eventDto.getCategory().getId() != null)) {
-            Long id = eventDto.getPlace().getId();
-            Long category = eventDto.getCategory().getId();
-            Long organizer = eventDto.getOrganizer().getId();
-            Long status = eventDto.getStatus().getId();
-            placeRepository.findById(id)
+            Long placeId = eventDto.getPlace().getId();
+            Long categoryId = eventDto.getCategory().getId();
+            Long organizerId = eventDto.getOrganizer().getId();
+            Long statusId = eventDto.getStatus().getId();
+            placeRepository.findById(placeId)
                     .ifPresent(place -> {
                         eventDto.setPlaceToDto(place);
-                        categoryRepository.findById(category)
+                        categoryRepository.findById(categoryId)
                                 .ifPresent(category1 -> {
                                     eventDto.setCategoryToDto(category1);
-                                    organizerRepository.findById(organizer)
+                                    organizerRepository.findById(organizerId)
                                             .ifPresent(organizer1 -> {
                                                 eventDto.setOrganizerToDto(organizer1);
-                                                statusRepository.findById(status)
+                                                statusRepository.findById(statusId)
                                                         .ifPresent(status1 -> {
                                                             eventDto.setStatusToDto(status1);
                                                             eventRepository.save(eventDto.toEvent());
